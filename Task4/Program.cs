54676b9388e6d60db29fc57158014fff6f74cc32
Koriakin Pavel <polo_kor@mail.ru>
2023-02-14 22:01:38 +0300

    4 Задачи

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(10, 10000);
+System.Console.WriteLine(number);
+
+int max = 0;
+
+while (number > 0)
+{
+    if (max < number % 10)
+    {
+        max = number % 10;
+    }
+    number /= 10;
+}
+
+System.Console.WriteLine($"Наибольшая цифра {max}");
+
