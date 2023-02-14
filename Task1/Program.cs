909c9912242a6b8cc282ced3f283517a7048f919
Koriakin Pavel <polo_kor@mail.ru>
2023-02-14 21:45:11 +0300

    3 Задачи

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int Prompt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int del = Prompt("Введите делитель  ");
+int num = Prompt("Введите число  ");
+int ost = num % del;
+if(ost == 0)
+{
+    System.Console.WriteLine($"{num} является кратным числу {del}");
+}
+else
+{
+    System.Console.WriteLine($"{num} не кратное {del}, остаток равен {ost}");
+}
